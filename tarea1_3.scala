371513fb865c731ae38927906175c8c1f2a0bda0
dannnniel <dannybacort@hotmail.com>
2015-09-11 20:29:36 -0500

    Termino la tarea 1_3

---
+++ tarea1_3.scala
@@ -0,0 +1,19 @@
+println("Ejercicio 3")
+println("Programa de tres personas que invierten su dinero")
+println("Cantidad1")
+val Cantidad1 = readFloat()
+println("Cantidad2")
+val Cantidad2 = readFloat()
+println("Cantidad3")
+val Cantidad3 = readFloat()
+val total = Cantidad1 + Cantidad2 + Cantidad3
+println("Porcentaje1")
+val porcentaje1 = Cantidad1 * 100/total
+print(porcentaje1)
+println("Porcentaje2")
+val porcentaje2 = Cantidad2 * 100/total
+print(porcentaje2)
+println("Porcentaje3")
+val porcentaje3 = Cantidad3 * 100/total
+print(porcentaje3)
+println("El programa ha terminado")
