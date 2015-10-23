485fb3171e25cf2d16147840b781e59b50b87ef0
dannnniel <dannybacort@hotmail.com>
2015-10-22 22:06:41 -0500

    Termino la tarea de la clase catalogo

---
+++ clase_catalogo.scala
@@ -0,0 +1,83 @@
+class Producto(n: String, p: Float, c: String){
+	val nombre = n
+	val precio = p
+	val codigo = c
+}
+
+class Catalogo{
+	val productos = new Array[Producto](50)
+	var cantidadProducto = 0
+	def agregar (producto: Producto ){
+		productos(cantidadProducto) = producto
+		cantidadProducto += 1
+	}
+}
+
+main()
+
+def main() {
+	catalogo ( new Catalogo )
+}
+
+def catalogo( catalogo : Catalogo ) {
+	var opcion = 3
+	do {
+		mostrarMenuPrincipal()
+		opcion = readInt()
+		opcion match {
+			case 1 => agregarProductos(catalogo)
+			case 2 => nota(catalogo)
+			case default => println("Adios...")
+		}
+		} while (opcion != 3)
+	}
+ 
+def mostrarMenuPrincipal() {
+	println("1.- Agregar productos")
+	println("2.- Nota")
+	println("3.- Salir")
+}
+
+def agregarProductos (catalogo : Catalogo) {
+	var continuar = 'N'
+	do {
+		capturarDatosProducto(catalogo)
+		mostrarCatalogoProductos(catalogo)
+		print("Quieres agregar otro producto (S/N)? ")
+		continuar = readChar()
+		} while (continuar == 'S')
+	}
+
+def capturarDatosProducto(catalogo : Catalogo) {
+	println("Ingresa los datos del producto")
+	print("Nombre : ")
+	var nombre = readLine()
+	println("Precio : ")
+	var precio = readFloat()
+	println("Codigo de barras : ")
+	var codigo = readLine()
+	catalogo.agregar(new Producto(nombre, precio, codigo))
+}
+
+def mostrarCatalogoProductos(catalogo : Catalogo) {
+	println("Nombre | Precio | Codigo")
+	for (j <- 0 to catalogo.cantidadProducto -1) {
+		println("|"+catalogo.productos(j).nombre + " |" +  catalogo.productos(j).precio + " | " + catalogo.productos(j).codigo)
+	}
+}
+
+def nota(catalogo: Catalogo){
+	var totalNota=0.0
+	var otro = 'N'
+	mostrarCatalogoProductos(catalogo)
+	do{
+	 println("Ingresa el numero del producto : ")
+	 var producto=readInt()
+	 println("Cantidad de productos : ")
+	 var cantidad=readInt()
+	 totalNota += catalogo.productos(producto - 1).precio * cantidad
+	 println("Agregar otro producto : (S/N) ")
+	 otro = readChar()
+	 }while(otro != 'N')
+	 println("Total a pagar : " + totalNota)
+	}
